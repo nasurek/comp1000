1f8902dfba7f8d9789ed91867280621e138ab3a1
nasurek <kchandrarooban@gmail.com>
2025-10-24 11:48:14 +0100

    Add files via upload

---
+++ finaltask2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int num1 = 2; 
+    int num2;
+
+    cout << "Enter an integer to multiply with 2. ";
+    cin >> num2;
+
+ int product = num1 * num2;
+  
+    cout << "The product of " << num1 << " and " << num2 << " is " << product << endl;
+    
+    return 0;
+}
