1639575ba5a3e14d9688985554f9bc0e2b52d0a2
nasurek <kchandrarooban@gmail.com>
2025-10-24 11:38:26 +0100

    Add files via upload

---
+++ finaltask1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+int num2 = 2;
+int num1 = 1;
+int sum = num1 + num2;
+
+cout << "What is your first integer? ";
+cin >> num1;
+
+cout << "What is your second integer? ";
+cin >> num2;
+
+cout << "The result of the two integers are " << sum;
+
+return 0;
+
+}
